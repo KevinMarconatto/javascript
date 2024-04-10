9001c51aa7f39187b85fa43aa4635d6961205634
Kevin Marconatto <104887641+KevinMarconatto@users.noreply.github.com>
2024-04-10 19:57:39 -0300

    exercícios

---
+++ aula14/ambiente2.js
@@ -0,0 +1,7 @@
+console.log('Quantidade de passos até o destino final:')
+
+for(var passo = 0; passo <= 50; passo++) {
+    console.log(`Passo ${passo}`)
+}
+
+console.log('Chegamos no destino final!')
